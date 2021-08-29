611b565b8cfe156c693cd2ea22ed551224b2a82e
Julia Schumacher <schumacher.julia@gmail.com>
2021-08-29 17:47:08 -0300

    Create rotes.js

---
+++ rotes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const CandidateController = require('./CandidateController.js');
+const routes = new express.Router();
+
+routes.post('/register', CandidateController.register);
+
+routes.get('/', (req, resp) => {
+    resp.send('Servidor online');
+})
+
+
+module.exports = routes;
